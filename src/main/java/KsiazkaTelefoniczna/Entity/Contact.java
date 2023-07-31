dd2c1d9ddfdfac9587102e287264d6aad9cb8ab9
Raulek123 <wywial.krzysztof@wp.pl>
2023-07-31 17:14:35 +0200

    Adding new classes to the project.

---
+++ src/main/java/KsiazkaTelefoniczna/Entity/Contact.java
@@ -0,0 +1,16 @@
+package KsiazkaTelefoniczna.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Contact {
+    private String name;
+    private int phoneNumber;
+
+    @Override
+    public String toString() {
+        return "Kontakt: " + name + ", numer telefonu: " + phoneNumber;
+    }
+}
