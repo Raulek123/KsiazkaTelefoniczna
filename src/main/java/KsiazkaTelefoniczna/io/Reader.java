b6cb6e851e2ae8cc6115ea8f4e92167ead2e1516
Raulek123 <wywial.krzysztof@wp.pl>
2023-08-02 20:42:18 +0200

    Improvements method getString.

--- src/main/java/KsiazkaTelefoniczna/io/Reader.java
+++ src/main/java/KsiazkaTelefoniczna/io/Reader.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Reader {
     private final Scanner sc = new Scanner(System.in);
+
     public int getInt() {
         try {
             return sc.nextInt();
@@ -13,7 +14,15 @@ public class Reader {
     }
 
     public String getString() {
-        return sc.nextLine();
+        String line = sc.nextLine();
+        if (line == null) {
+            throw new NullPointerException("Cannot be null");
+        }
+        if (line.isEmpty()) {
+            throw new IllegalArgumentException("Cannot be empty");
+        } else {
+            return line;
+        }
     }
 
     public void close() {
